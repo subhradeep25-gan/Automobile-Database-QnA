13b3d80458071be920ab013ef1c2a4c0138ea60b
subhradeep25-gan <subhradeep.21ug2060@iiitranchi.ac.in>
2024-01-01 21:54:38 +0530

    Added langchain and streamlit code

---
+++ main.py
@@ -0,0 +1,11 @@
+from langchain_helper import get_few_shot_db_chain
+import streamlit as st
+
+st.title("E-Commerce: DataBase QnA 🛒")
+
+question = st.text_input("Question: ")
+if question:
+    chain = get_few_shot_db_chain()
+    answer = chain.run(question)
+    st.header("Answer:")
+    st.write(answer)
